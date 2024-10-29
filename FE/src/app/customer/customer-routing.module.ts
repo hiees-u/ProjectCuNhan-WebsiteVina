b684a1762c0d62bf74cc6b3ed84e85b32136797e
hiees-u <nguyenminhhieu.31018@gmail.com>
2024-10-29 22:42:52 +0700

    add cart detail

--- FE/src/app/customer/customer-routing.module.ts
+++ FE/src/app/customer/customer-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { ViewProductsComponent } from './view-products/view-products.component';
 import { OrderProductsComponent } from './order-products/order-products.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,8 @@ const routes: Routes = [
     children: [
       { path: 'view-product', component: ViewProductsComponent},
       { path: 'order-product', component: OrderProductsComponent},
+      { path: 'cart-details', component: CartDetailComponent},
+      { path: 'user-details', component: UserDetailComponent},
       { path: '', redirectTo: 'view-product', pathMatch: 'full'}
     ]
   }
